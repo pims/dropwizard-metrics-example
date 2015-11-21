a3d99d5702f892938b03bd2c17702d32ae374932
Tim Bart <tim@sayhello.com>
2015-11-21 12:24:19 -0800

    Adding @Timed annotation to monitor endpoint

--- src/main/java/com/example/application/ExampleResource.java
+++ src/main/java/com/example/application/ExampleResource.java
@@ -2,6 +2,7 @@ package com.example.application;
 
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.annotation.Timed;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,6 +17,7 @@ public class ExampleResource {
         this.computationReturnsFalse = mr.meter("computation-returns-false");
     }
 
+    @Timed
     @GET
     public Response doSomething() {
         boolean result = computation();
